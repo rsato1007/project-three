3cf2eccb0b45ec05e0ece99191752e9e91e528ed
rose belmont <rose.daemon.belmont@gmail.com>
2021-09-07 19:02:17 -0700

    adds bcrypt password hashing

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const SALT_ROUNDS = 6;
 
 const schema = new mongoose.Schema ({
     Name: {type: string, required: true},
@@ -7,5 +10,17 @@ const schema = new mongoose.Schema ({
     Bio: {type: string},
 });
 
+schema.pre("save", function (next) {
+    const user = this;
+    if (!user.isModified("Password")) return next();
+    //changed password, re-hash, and salt to taste
+    bcrypt.hash(user.Password, SALT_ROUNDS, function (err, hash) {
+        if (err) return next(err);
+        //replace plaintext with salty hash
+        user.Password = hash;
+        next();
+    });
+});
 
-export default schema;
+const User = mongoose.model("User", schema);
+module.exports = User;
