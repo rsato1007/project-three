aa8c78d58024ab1959f7352a99d02ade2c624d27
rose belmont <rose.daemon.belmont@gmail.com>
2021-09-07 20:46:34 -0700

    refactor of components/index.js

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Email from "./Email_Submit";
+import LogButton from "./Email_Submit"
+import Username_Password from "./Username_Password";
+
+module.exports = {
+    Email,
+    LogButton,
+    Username_Password,
+}
+
