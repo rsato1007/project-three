82486ae66531cc4267067e06203e4c9bbcf51209
rose belmont <rose.daemon.belmont@gmail.com>
2021-09-08 11:45:18 -0700

    merge with main adds framework for login page

---
+++ src/pages/LoginPage/index.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { Username_Password, Email, Submit  } from "../../components";
+//forms for login page
+const LoginPage = () => {
+    //allows form to set username and password
+    const [Name, setName] = useState("");
+    const [Password, setPassword] = useState("");
+
+    const handleSubmit = async () => {
+        const user = {
+            Name,
+            Password,
+        };
+        console.log(user);
+        //fill with relevant code when hooked to back-end user api.
+    };
+    
+}
