e0bc95f4fbeb608b34e6bdb8b80a5630a649ee20
Chris Ladd <codebyladd@gmail.com>
2021-09-08 21:17:24 -0600

    add update,updatecomment,destroy, destroycomment to post.js in controller

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -74,11 +74,76 @@ const createComment = (req, res) => {
         .catch((err) => console.log(err));
 };
 
+// Update(Posts)- put - (id)
+const update = (req, res) => {
+    db.Post.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true },
+        (err, updatePost) => {
+            if (err) console.log("Error in Posts#update:", err);
+
+            return res.status(202).json({
+                data: updatePost,
+            });
+        }
+    );
+};
+
 //Update - Put - Comments(Edit)
 const updateComment = (req, res) => {
     db.Post.findById(req.params.id).then((foundPost) => {
         if (!foundPost) return console.log("Error in Comment#update");
 
-        const commentById = foundPost.comments.id(req.params.co)
-    })
-}
+        const commentById = foundPost.comments.id(req.params.commentId);
+        commentById.author = req.body.author;
+        commentById.body = req.body.body;
+        foundPost.save();
+
+        return res.status(202).json({
+            message: "Success",
+            data: commentById,
+        });
+    });
+};
+
+// Remove - Delete - (ID)
+
+const destroy = (req, res) => {
+    db.Post.findByIdAndDelete(req.params.id, (err, deletedPost) => {
+        if (err) console.log("Error in Posts#destroy:", err);
+        
+        return res.status(200).json({
+            data: deletedPost,
+        });
+    });
+};
+
+//Remove - delete- For comments
+const destroyComment = (req, res) => {
+    db.Post.findById(req.params.id).then((foundPost) => {
+        if (!foundPost) return console.log("error in comment#destroy");
+
+        const commentById = foundPost.comments.id(req.params.commentId);
+        console.log(commentId);
+        commentById.remove();
+        foundPost.save();
+
+        return res.status(200).json({
+            message: "Success",
+            data: commentById,
+        });
+    });
+};
+
+module.exports = {
+    index,
+    show,
+    showComments,
+    create,
+    createComment,
+    update,
+    updateComment,
+    destroy,
+    destroyComment,
+};
