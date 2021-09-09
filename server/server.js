2ead3ac9f929329269f6d8264f4152a7eda6800d
Chris Ladd <codebyladd@gmail.com>
2021-09-08 22:07:52 -0600

    rename routes/posts to activate site

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 /* Middleware */
 //Cors
-// app.use(cors());
+app.use(cors());
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 app.use(express.json());
