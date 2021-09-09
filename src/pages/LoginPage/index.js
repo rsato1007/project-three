60bce10d5a0687648ea01394ef678541ec9ed2eb
rose belmont <rose.daemon.belmont@gmail.com>
2021-09-08 18:39:44 -0700

    refactors login to be one component

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,25 +1,58 @@
 import React, { useState } from "react";
-import { Username_Password, Email, Submit  } from "../../components";
-//forms for login page
-const LoginPage = () => {
-    //allows form to set username and password
+import { useHistory } from "react-router-dom";
+import { Username_Password } from "../../components";
+import "./styles.css";
+//still needs token and user imports
+
+const login = () => {
+    const history = useHistory();
     const [Name, setName] = useState("");
     const [Password, setPassword] = useState("");
 
+    //submit function
     const handleSubmit = async () => {
         const user = {
             Name,
             Password,
         };
-        console.log(user);
-        //fill with relevant code when hooked to back-end user api.
+
+        //interacts with user api to generate token
+        const res = await User.login(user);
+        if (res.data.data) {
+            const token = res.data.data.token;
+            console.log("Login made this! ", token);
+            setToken(token);
+            setName("");
+            setPassword("");
+            history.push("/");
+        }
+        else {
+            alert("Oops! All Errors!");
+        }
     };
-    
-    //what actually gets rendered on the page
+
+    //what actually gets rendered to the user
     return (
-        <div className="login-form">
-            <Username_Password setName={setName} setPassword={setPassword} />
-            <Submit onClick={handleSubmit} buttonText="test" />
+        <div className="Login-inputs">
+            <input 
+                className="Name"
+                type="username"
+                onChange={(e) => setName(e.target.value)}
+                value={Name}
+                title="username"
+                placeholder="Username"
+            />
+            <input 
+                className="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={Password}
+                title="password"
+                placeholder={"Password"}
+            />
+            <button className="submit" onClick={handleSubmit}>Login</button>
+
         </div>
-    )
-}
+    );
+};
+
+export default login;
