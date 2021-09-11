937be305179b36e5ea8f1d783104c22cdf95202e
rsato1007 <robertmsato@gmail.com>
2021-09-11 14:16:16 -0600

    beginning to connect login to backend

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,21 +1,43 @@
 import React, { useState } from 'react';
+import * as UserActions from "../../api/UserActions";
 
 const LoginForm = () => {
     //allows form to set username and password
     const [Name, setName] = useState("");
     const [Password, setPassword] = useState("");
 
-    const handleSubmit = async () => {
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
         const user = {
             Name,
             Password,
         };
-        console.log(user);
+
         //fill with relevant code when hooked to back-end user api.
+        const res = await UserActions.login(user);
+
+        console.log(res);
     };
 
     return (
-        <div>Testing LoginPage Works!</div>
+        <form className="login-inputs" onSubmit={(e) => handleSubmit(e)}>
+                        <input 
+                onChange={(e) => setName(e.target.value)}
+                value={Name}
+                type="text"
+                name="Name"
+                placeholder="USERNAME"
+            />
+            <input 
+                onChange={(e) => setPassword(e.target.value)}
+                value={Password}
+                type="password"
+                name="Password"
+                placeholder="PASSWORD"
+            />
+            <button type="submit">Login</button>
+        </form>
     )
 };
 
