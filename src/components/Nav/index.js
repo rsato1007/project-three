5b63f8b149f6b112e3c556a0b82b78d6266302a5
rsato1007 <robertmsato@gmail.com>
2021-09-11 13:31:08 -0600

    adding nav to pages

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,11 +1,37 @@
 import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import SignupPage from "../../pages/SignUpPage";
+import LoginPage from "../../pages/LoginPage";
 
-const Nav = () => {
-    return (
-        <div>
-            Testing
-        </div>
-    )
+class Nav extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="commons-nav">
+                    {/* Using as base example */}
+                    <Link to="/">
+                        Home
+                    </Link>
+                </div>
+                <Switch>
+                    {/* Using as base example */}
+                    <Route
+                        exact
+                        path="/"
+                        render={(props) => <SignupPage {...props} />}
+                    />
+                </Switch>
+                <Switch>
+                    {/* Using as base example */}
+                    <Route
+                        exact
+                        path="/login"
+                        render={(props) => <LoginPage {...props} />}
+                    />
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default Nav;
