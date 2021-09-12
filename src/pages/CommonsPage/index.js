3ccaea2deb04a415050f1ce530e0cd5c81ea5df4
rsato1007 <robertmsato@gmail.com>
2021-09-11 21:04:00 -0600

    adding commons page

---
+++ src/pages/CommonsPage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CommonsPage = () => {
+    return (
+        <div>
+            Welcome to the commons page!
+        </div>
+    )
+}
+
+export default CommonsPage
