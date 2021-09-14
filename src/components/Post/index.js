5008b8a2af97e16c50ea6c503ab75e0b1ff0b314
rsato1007 <robertmsato@gmail.com>
2021-09-13 20:15:45 -0600

    making post data come from commons state:

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -3,9 +3,9 @@ import React from "react";
 const Post = ({Body, Author, Date}) => {
     return (
         <div className='post-contents'>
-            <div>I went to the beach this weeked it was dope!</div>
-            <div>By Robert Sato</div>
-            <div>Date Posted: 09/11/2021</div>
+            <div>{Body}</div>
+            <div>{Author}</div>
+            <div>{Date}</div>
         </div>
     )
 }
--- src/pages/CommonsPage/index.js
+++ src/pages/CommonsPage/index.js
@@ -4,18 +4,20 @@ import Post from "../../components/Post";
 
 const CommonsPage = () => {
     const [Name, setName] = useState("");
-    const [Posts, setPosts] = useState('');
+    const [Posts, setPosts] = useState([]);
 
     /* Todo: Write a function that gathers all the information from friend posts */
 
     useEffect(() => {
         const tokenInfo = getUserFromToken();
         setName(tokenInfo.Name);
+        // console.log("Here's posts", postCollection);
         /* This is emulating a simplied version of actual friends posts before we implement data. */
-        setPosts([
+        setPosts([...Posts, 
             {Body: "I went to the beach this weekend.", Author: "tacoDog", Date: "09/11/2021"},
-            {}
-        ])
+            {Body: "Go Sports Team", Author: "Paper John", Date: "09/12/2021"}
+        ]);
+        console.log(Posts);
     },[]);
     return (
         <div className="commons-page">
@@ -23,7 +25,11 @@ const CommonsPage = () => {
             <div>
                 Welcome Back {Name}!
             </div>
-            <Post />
+            {Posts.map((post) => {
+                return (
+                    <Post Body={post.Body} Author={post.Author} Date={post.Date}/>
+                )
+            })}
         </div>
     )
 }
