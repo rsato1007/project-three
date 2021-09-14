d9269d19aa4b17c987b217464667257418f1cae4
rsato1007 <robertmsato@gmail.com>
2021-09-13 20:37:50 -0600

    creating PostForm component

---
+++ src/components/PostForm/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PostForm = () => {
+    return (
+        <div>Make a post!</div>
+    )
+}
+
+export default PostForm
--- src/pages/CommonsPage/index.js
+++ src/pages/CommonsPage/index.js
@@ -1,23 +1,26 @@
 import React, {useEffect, useState} from "react";
 import { getUserFromToken } from "../../Tools/TokenAction";
 import Post from "../../components/Post";
+import PostForm from "../../components/PostForm";
 
 const CommonsPage = () => {
     const [Name, setName] = useState("");
     const [Posts, setPosts] = useState([]);
+
+    const getPosts = async () => {
+
+    }
 
     /* Todo: Write a function that gathers all the information from friend posts */
 
     useEffect(() => {
         const tokenInfo = getUserFromToken();
         setName(tokenInfo.Name);
-        // console.log("Here's posts", postCollection);
         /* This is emulating a simplied version of actual friends posts before we implement data. */
         setPosts([...Posts, 
             {Body: "I went to the beach this weekend.", Author: "tacoDog", Date: "09/11/2021"},
             {Body: "Go Sports Team", Author: "Paper John", Date: "09/12/2021"}
         ]);
-        console.log(Posts);
     },[]);
     return (
         <div className="commons-page">
@@ -25,6 +28,7 @@ const CommonsPage = () => {
             <div>
                 Welcome Back {Name}!
             </div>
+            <PostForm />
             {Posts.map((post) => {
                 return (
                     <Post Body={post.Body} Author={post.Author} Date={post.Date}/>
