5c0e43cc4c36844ab5da175bfb8ce920f331ace9
rsato1007 <robertmsato@gmail.com>
2021-09-15 19:28:40 -0600

    making basics of gifs frontend

--- src/App.js
+++ src/App.js
@@ -2,13 +2,6 @@ import { urlencoded } from 'body-parser';
 import React from 'react';
 import SignupPage from './pages/SignUpPage';
 
-
-/* This file is used to test the API before we try to incorporate it into our code */
-const apiKey = "VR5MXJ8fw0BYcpei2IQf0AUzzmtXirfd";
-const searchTerm = 'Ice Cream';
-/* How many searchTerms should we include? */
-const fetchURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&limit=20&offset=0&rating=g&lang=en`;
-
 function App() {
   
 
---
+++ src/api/GifActions.js
@@ -0,0 +1,9 @@
+import CommonsCommands from "./CommonsCommands";
+
+// Get gifs
+const get = (data) => {
+    console.log("Grabbing gifs");
+    // return CommonsCommands.get(/*POST ID IN HERE*/);
+};
+
+export { get };
