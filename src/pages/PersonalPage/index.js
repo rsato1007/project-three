d15e93d34af7d5b328194c092dd87cb353cd5ff9
Chris Ladd <codebyladd@gmail.com>
2021-09-15 22:12:44 -0600

    PersonalPage post rendering

--- src/pages/PersonalPage/index.js
+++ src/pages/PersonalPage/index.js
@@ -3,8 +3,10 @@ import { getUserFromToken } from "../../Tools/TokenAction";
 import Post from "../../components/Post";
 import PostForm from "../../components/PostForm";
 import * as PostActions from "../../api/PostActions";
+import { TokenExpiredError } from "jsonwebtoken";
 
 const PersonalPage = () => {
+    const [Token, setToken] = useState(getUserFromToken());
     const [Posts, setPosts] = useState([]);
 
     const getPosts = async (event) => {
@@ -15,4 +17,22 @@ const PersonalPage = () => {
         const res = await PostActions.getAllPostsByAuthor();
         setPosts(res.data.data);
     }
+    useEffect(() => {
+        getPosts();
+    }, []);
+    return (
+        <div className="personal-page">
+            <div className="title-pp">
+                {Token.Name} Commons Page!
+            </div>
+            <PostForm Token={Token} getPosts={(e) => getPosts(e)}/>
+            {Posts.map((post) => {
+                return (
+                    <Post Body={post.body} Author={post.author} Date={post.date} Token={Token} id={post._id} getPosts={getPosts}/>
+                )
+            })}
+        </div>
+    )
 }
+
+export default PersonalPage
