5dc70f82ea4e1b4c69abc98942cca42cdd9a4ded
rose belmont <rose.daemon.belmont@gmail.com>
2021-09-15 18:28:55 -0700

    adds like buttons, cant go below 0

---
+++ src/components/likes/index.js
@@ -0,0 +1,29 @@
+//add style sheet
+
+import React, { useState, useEffect } from "react";
+
+const Likes = () => {
+    const [likes, setLikes] = useState(0);
+
+    useEffect(() => {
+        console.log("liked")
+    }, [likes]);
+
+
+    //what gets rendered
+    return (
+        <div>
+            <button onClick={() => setLikes(likes + 1)}
+            >Like</button>
+            <button onClick={() => {
+                if (likes !== 0) {
+                    setLikes(likes - 1);
+                };
+                //have something so user knows it cant go below 0
+            }}>Dislike</button>
+            <p>This post has been liked by {likes} Users</p>
+        </div>
+    );
+};
+
+export default Likes;
