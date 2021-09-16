dcd9022e23ee35ce7a275f07e87832f514abbb00
Chris Ladd <codebyladd@gmail.com>
2021-09-15 21:00:43 -0600

    PersonalPage layout

---
+++ src/pages/PersonalPage/index.js
@@ -0,0 +1,9 @@
+import React, {useEffect, useState} from "react";
+import { getUserFromToken } from "../../Tools/TokenAction";
+import Post from "../../components/Post";
+import PostForm from "../../components/PostForm";
+import * as PostActions from "../../api/PostActions";
+
+const PersonalPage = () => {
+    
+}
