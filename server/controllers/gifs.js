0cf9fc0c0854d7c5bdcf923f464c2532e7a1610d
rsato1007 <robertmsato@gmail.com>
2021-09-16 23:21:07 -0600

    incorporating gif api into app

--- server/controllers/gifs.js
+++ server/controllers/gifs.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const index = async (req, res) => {
     // Relevant Variables
 
-    axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${req.body.searchTerm}&limit=20&offset=0&rating=g&lang=en`)
+    axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${req.body.searchTerm}&limit=${req.body.limit}&offset=0&rating=g&lang=en`)
         .then((response) => {
             return res.status(200).json({
                 message: "Success",
--- src/components/GifFinder/index.js
+++ src/components/GifFinder/index.js
@@ -3,33 +3,42 @@ import * as GifActions from "../../api/GifActions";
 
 const GifFinder = () => {
     const [search, setSearch] = useState("");
+    const [gifNum, setGifNum] = useState(6);
     const [apiData, setApiData] = React.useState([]);
 
     const grabGifs = async (event) => {
         event.preventDefault();
         const gifInfo = {
             searchTerm: search,
+            limit: gifNum
         }
         const res = await GifActions.getGifs(gifInfo);
 
         if (res.data.data.data) {
             setApiData(res.data.data.data);
-            console.log("Here's what the backend sent:", res.data.data.data);
+            setSearch("");
         }
     }
     // Write a function that goes into the backend to grab the API information.
 
     return (
-        <form onSubmit={(e) => grabGifs(e)}>
-            <input 
-                onChange={(e) => setSearch(e.target.value)}
-                value={search}
-                type="text"
-                name="search"
-                placeholder="Search for gif"
-            />
-            <button type="submit">Search</button>
-        </form>
+        <div>
+            <form onSubmit={(e) => grabGifs(e)}>
+                <input 
+                    onChange={(e) => setSearch(e.target.value)}
+                    value={search}
+                    type="text"
+                    name="search"
+                    placeholder="Search for gif"
+                />
+                <button type="submit">Search</button>
+            </form>
+            {apiData.map((gif) => {
+                return (
+                    <img src={gif.url}/>
+                )
+            })}
+        </div>
     )
 }
 
